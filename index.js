be2a26d4cfd267d16e3845185e76793c574ce13d
Fernando D'abenigno <dabenignofernando32@gmail.com>
2022-05-05 21:14:13 -0300

    Primer entrega del proyecto final

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-let nombreIngresado = prompt("Ingrese su Nombre");
-alert(`Hola ${nombreIngresado}. Bienvenid/a, solamente hacemos envios a Buenos aires, Chaco y Misiones.`);
-
-function CalcularEnvio(){
-    let provincia = prompt("A donde queres hacer el envio?")
-    
-    while (provincia != "Buenos Aires" || provincia != "Chaco" || provincia != "Misiones"){
-
-        if(provincia === "Buenos aires") {
-            alert("El envio te saldra $3000, llegara en 3 dias habiles");
-            break;
-        }
-        else if(provincia === "Chaco") {
-            alert("El envio te saldra $3500, llegara en 4 dias habiles");
-            break;
-        } else if(provincia === "Misiones") {
-            alert("El envio te saldra $4000, llegara en 5 dias habiles");
-            break;
-        }
+let consolaPlaystation = [
+    {nombre: "PlayStation1", precio: 2000},
+    {nombre: "PlayStation2", precio: 4000},
+    {nombre: "PlayStation3", precio: 30000},
+    {nombre: "PlayStation4", precio: 50000},
+    {nombre: "PlayStation5", precio: 90000},
+]
+function operaciones(operacion){
+    if(operacion === "encontrar"){
+        return (consola) => consolaPlaystation.find(x => x.nombre === consola)
     }
-    
+    if(operacion === "filtrar"){
+        return (precio) => consolaPlaystation.filter(x => x.precio > precio)
+    }
 }
-CalcularEnvio()
+let filtro = operaciones ("filtrar")
+console.log(filtro(2000));
 
-
-
+let consolaXbox = [
+    {nombre: "Xbox 360", precio: 5000},
+    {nombre: "Xbox one", precio: 7000},
+    {nombre: "Xbox series x", precio: 40000},
+    {nombre: "Xbox series s", precio: 50000},
+]
+function comprar(comprar){
+    if(comprar === "encontrar"){
+        return (consola) => consolaXbox.find(x => x.nombre === consola)
+    }
+    if(comprar === "filtrar"){
+        return (precio) => consolaXbox.filter(x => x.precio > precio)
+    }
+}
+let filt = comprar ("filtrar")
+console.log(filt(5000));
