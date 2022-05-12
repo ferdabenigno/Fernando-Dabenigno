7976bbbe169cc9681e9a87bfa381d64b61c19c30
Fernando D'abenigno <dabenignofernando32@gmail.com>
2022-05-12 00:10:10 -0300

    Primer entrega del proyecto final

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 let consolaPlaystation = [
-    {nombre: "PlayStation1", precio: 2000},
-    {nombre: "PlayStation2", precio: 4000},
-    {nombre: "PlayStation3", precio: 30000},
-    {nombre: "PlayStation4", precio: 50000},
-    {nombre: "PlayStation5", precio: 90000},
+    {nombre: "Playstation1", precio: 2000},
+    {nombre: "Playstation2", precio: 4000},
+    {nombre: "Playstation3", precio: 30000},
+    {nombre: "Playstation4", precio: 50000},
+    {nombre: "Playstation5", precio: 90000},
 ]
+
+let plays = prompt ("ingrese la play que desea llevar y le diremos el precio.");
+let playSeleccionada = consolaPlaystation.find(x => x.nombre === plays);
+let resultado = `${playSeleccionada.precio}`; 
+alert(resultado)
+
 function operaciones(operacion){
     if(operacion === "encontrar"){
         return (consola) => consolaPlaystation.find(x => x.nombre === consola)
@@ -13,6 +19,7 @@ function operaciones(operacion){
         return (precio) => consolaPlaystation.filter(x => x.precio > precio)
     }
 }
+
 let filtro = operaciones ("filtrar")
 console.log(filtro(2000));
 
@@ -22,6 +29,12 @@ let consolaXbox = [
     {nombre: "Xbox series x", precio: 40000},
     {nombre: "Xbox series s", precio: 50000},
 ]
+
+let xbox = prompt ("ingrese la xbox que desea llevar y le diremos el precio.");
+let xboxSeleccionada = consolaXbox.find(x => x.nombre === xbox);
+let resultadoxbox = `${xboxSeleccionada.precio}`; 
+alert(resultadoxbox)
+
 function comprar(comprar){
     if(comprar === "encontrar"){
         return (consola) => consolaXbox.find(x => x.nombre === consola)
