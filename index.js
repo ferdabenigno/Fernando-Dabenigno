82aeb64af1ff8c7cba46896b72e436260f743451
Fernando D'abenigno <dabenignofernando32@gmail.com>
2022-05-19 23:01:09 -0300

    segunda entrega del proyecto final

--- index.js
+++ index.js
@@ -1,54 +1,101 @@
-/*let consolaPlaystation = [
-    {nombre: "Playstation1", precio: 2000},
-    {nombre: "Playstation2", precio: 4000},
-    {nombre: "Playstation3", precio: 30000},
-    {nombre: "Playstation4", precio: 50000},
-    {nombre: "Playstation5", precio: 90000},
-]
 
-let plays = prompt ("ingrese la play que desea llevar y le diremos el precio.");
-let playSeleccionada = consolaPlaystation.find(x => x.nombre === plays);
-let resultado = `${playSeleccionada.precio}`; 
-alert(resultado)
+const contenedor = document.getElementById("consolas");
+const tablaCarrito = document.getElementById("tablaCarrito")
+const carrito = localStorage.getItem("carrito") || [];
 
-function operaciones(operacion){
-    if(operacion === "encontrar"){
-        return (consola) => consolaPlaystation.find(x => x.nombre === consola)
+const CONSOLAS = [
+    {
+        id: 1,
+        nombre: "Playstation3",
+        precio:  30000,
+        stock: 4,
+        imagen: "https://http2.mlstatic.com/D_NQ_NP_808574-MLA32706766958_102019-O.webp"
+    },
+    {
+        id: 2,
+        nombre: "Playstation4",
+        precio: 50000,
+        stock: 10,
+        imagen: "https://http2.mlstatic.com/D_NQ_NP_690761-MLA49613056842_042022-O.webp"
+    },
+    {
+        id: 3,
+        nombre: "Playstation5",
+        precio: 200000,
+        stock: 1,
+        imagen: "https://www.naldo.com.ar/medias/504562.jpg-515Wx515H?context=bWFzdGVyfHJvb3R8NjgxNzN8aW1hZ2UvanBlZ3xoNDQvaGViLzk1NTAwNzAwMjIxNzQuanBnfGIxNTQ2OGIzYzU5MDgwNzI3YzE0ZTAxZjU0NGMwYzFlY2I2MmUzODg1Y2ViNzk5MDRmMTRjODQ5Y2M4YTY0N2I"
+    },
+    {
+        id: 4,
+        nombre: "Xbox 360",
+        precio: 70000,
+        stock: 2,
+        imagen: "https://http2.mlstatic.com/D_NQ_NP_749276-MLA32731207914_112019-O.webp"
+    },
+    {
+        id: 5,
+        nombre: "Xbox series s",
+        precio: 100000,
+        stock: 0,
+        imagen: "https://http2.mlstatic.com/D_NQ_NP_627149-MLA44484230438_012021-O.webp"
     }
-    if(operacion === "filtrar"){
-        return (precio) => consolaPlaystation.filter(x => x.precio > precio)
-    }
+];
+
+localStorage.setItem("juegos", JSON.stringify(CONSOLAS));
+
+
+const getCard = (item) => {
+    return (
+        `
+        <div class="card" style="width: 18rem;">
+            <img src="${item.imagen}" class="card-img-top" alt="${item.nombre}">
+            <div class="card-body">
+                <h5 class="card-title">${item.nombre}</h5>
+                <p class="card-text">${item.precio}</p>
+                <p class="card-text">stock: ${item.stock}</p>
+                <a href="#" onclick="agregarCarrito(${item.id})" class="btn btn-primary">Agregar al carrito</a>
+        </div>
+            </div>
+    `);
+};
+const getRow = (item) => {
+    return (
+        `
+        <tr>
+            <th scope="row">${item.id}</th>
+            <td>${item.nombre}</td>
+            <td>${item.cantidad}</td>
+            <td>${item.precio}</td>
+            <td><img style="width:20px" src="${item.imagen} alt="imagen"></td>
+        </tr>
+        
+        `
+    )
 }
 
-let filtro = operaciones ("filtrar")
-console.log(filtro(2000));
+const cargarConsolas = (datos, nodo, esTabla) => {
+    let acumulador = ""
+    datos.forEach((el) => {
+        acumulador += esTabla ? getRow(el): getCard(el) ;
 
-let consolaXbox = [
-    {nombre: "Xbox 360", precio: 5000},
-    {nombre: "Xbox one", precio: 7000},
-    {nombre: "Xbox series x", precio: 40000},
-    {nombre: "Xbox series s", precio: 50000},
-]
-
-let xbox = prompt ("ingrese la xbox que desea llevar y le diremos el precio.");
-let xboxSeleccionada = consolaXbox.find(x => x.nombre === xbox);
-let resultadoxbox = `${xboxSeleccionada.precio}`; 
-alert(resultadoxbox)
-
-function comprar(comprar){
-    if(comprar === "encontrar"){
-        return (consola) => consolaXbox.find(x => x.nombre === consola)
+    })
+    nodo.innerHTML = acumulador;
+};
+const agregarCarrito = (id) => {
+    const seleccion = CONSOLAS.find(item => item.id === id);
+    const busqueda = carrito.findIndex(el => el.id === id);
+    if (busquedas === -1){
+        carrito.push({
+            id: seleccion.id,
+            nombre: seleccion.nombre,
+            precio: seleccion.precio,
+            cantidad: 1,
+            imagen: seleccion.imagen,
+        })
+    } else {
+        carrito[busqueda].cantidad = carrito[busqueda].cantidad + 1
     }
-    if(comprar === "filtrar"){
-        return (precio) => consolaXbox.filter(x => x.precio > precio)
-    }
+    cargarConsolas(carrito, tablaCarrito, true);
 }
-let filt = comprar ("filtrar")
-console.log(filt(5000));*/
-let formulario = document.getElementById("form");
-formulario.addEventListener("submit", function(e){
-    e.preventDefault();
-    let mail = document.getElementById("email-form").value;
-    console.log(mail);
 
-})
+cargarConsolas (CONSOLAS, contenedor, false);
